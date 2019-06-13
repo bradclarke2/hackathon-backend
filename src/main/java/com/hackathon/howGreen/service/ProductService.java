4f37502f2b4f6481465baf295fbe49d7bda3d0d0
bradleyclarke <bradley.j.clarke@tesco.com>
2019-06-13 19:30:57 +0100

    refactored calculation

--- src/main/java/com/hackathon/howGreen/service/ProductService.java
+++ src/main/java/com/hackathon/howGreen/service/ProductService.java
@@ -15,53 +15,17 @@ public class ProductService {
     @Autowired
     ProductRepository productRepository;
 
+    @Autowired
+    CalculationService calculationService;
+
     public Score getProductScore(String ean) {
         GreenInformation greenInformation = productRepository.findById(ean).get().getGreenInformation();
-        return calculateScore(greenInformation);
+        return calculationService.calculateScore(greenInformation);
     }
 
     public ProductInformation getProductInformation(String ean) {
         ProductInformation productInformation = productRepository.findById(ean).get();
         productInformation.getGreenInformation().setProductScore(getProductScore(ean));
         return productInformation;
-    }
-
-    private Score calculateScore(GreenInformation greenInformation){
-
-        int airMilesFromSource = greenInformation.getAirMiles().getFromSource();
-        int airMilesFromItems = greenInformation.getAirMiles().getItemsInProduct();
-        double airMilesAverage = (airMilesFromItems + airMilesFromSource) / 2;
-
-        int packagingRecylavility = greenInformation.getPackaging().getRecyclability();
-        int packagingSustainablePackaging  = greenInformation.getPackaging().getSustustainablePackaging();
-        double packagingAverage = (packagingRecylavility + packagingSustainablePackaging) / 2;
-
-        ArrayList<Boolean> list = new ArrayList<>();
-
-        if (greenInformation.getAccreditations().getFairTrade()!=null) {
-            list.add(greenInformation.getAccreditations().getFairTrade());
-        }
-
-        if (greenInformation.getAccreditations().getRainforestAlliance()!=null) {
-            list.add(greenInformation.getAccreditations().getFairTrade());
-        }
-
-        if (greenInformation.getAccreditations().getMarineStewardshipCouncil()!=null) {
-            list.add(greenInformation.getAccreditations().getFairTrade());
-        }
-
-        double score = list.stream()
-                .filter(item -> item.equals(true))
-                .count();
-
-        double avScore = score / list.size();
-
-        double airmilesWeighting = airMilesAverage * 0.4;
-        double packagingWeighting = packagingAverage * 0.4;
-        double accredWeighting = avScore * 0.2;
-
-        int finalScore = (int) (airmilesWeighting + packagingWeighting + accredWeighting);
-
-        return new Score(finalScore);
     }
 }
