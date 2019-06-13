6c283bd3fc0d9e5f3fd4771219a5714a765d2f1f
Sebastian Lavender <sebastianlavender@outlook.com>
2019-06-13 17:37:21 +0100

    Add score calculation

--- src/main/java/com/hackathon/howGreen/service/ProductService.java
+++ src/main/java/com/hackathon/howGreen/service/ProductService.java
@@ -7,6 +7,8 @@ import com.hackathon.howGreen.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class ProductService {
 
@@ -25,7 +27,41 @@ public class ProductService {
     }
 
     private Score calculateScore(GreenInformation greenInformation){
-        //TODO add model for calculation here
-        return new Score(10);
+
+        int airMilesFromSource = greenInformation.getAirMiles().getFromSource();
+        int airMilesFromItems = greenInformation.getAirMiles().getItemsInProduct();
+        double airMilesAverage = (airMilesFromItems + airMilesFromSource) / 2;
+
+        int packagingRecylavility = greenInformation.getPackaging().getRecyclability();
+        int packagingSustainablePackaging  = greenInformation.getPackaging().getSustustainablePackaging();
+        double packagingAverage = (packagingRecylavility + packagingSustainablePackaging) / 2;
+
+        ArrayList<Boolean> list = new ArrayList<>();
+
+        if (greenInformation.getAccreditations().getFairTrade()!=null) {
+            list.add(greenInformation.getAccreditations().getFairTrade());
+        }
+
+        if (greenInformation.getAccreditations().getRainforestAlliance()!=null) {
+            list.add(greenInformation.getAccreditations().getFairTrade());
+        }
+
+        if (greenInformation.getAccreditations().getMarineStewardshipCouncil()!=null) {
+            list.add(greenInformation.getAccreditations().getFairTrade());
+        }
+
+        double score = list.stream()
+                .filter(item -> item.equals(true))
+                .count();
+
+        double avScore = score / list.size();
+
+        double airmilesWeighting = airMilesAverage * 0.4;
+        double packagingWeighting = packagingAverage * 0.4;
+        double accredWeighting = avScore * 0.2;
+
+        int finalScore = (int) (airmilesWeighting + packagingWeighting + accredWeighting);
+
+        return new Score(finalScore);
     }
 }
