9189c89cf56552c4a7771c76f8c1ca99d676d5c1
bradleyclarke <bradley.j.clarke@tesco.com>
2019-06-13 15:42:28 +0100

    add basket score

--- src/main/java/com/hackathon/howGreen/api/BasketController.java
+++ src/main/java/com/hackathon/howGreen/api/BasketController.java
@@ -1,6 +1,7 @@
 package com.hackathon.howGreen.api;
 
 import com.hackathon.howGreen.domain.Basket;
+import com.hackathon.howGreen.domain.Score;
 import com.hackathon.howGreen.service.BasketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +19,17 @@ public class BasketController {
     }
 
     @GetMapping("{basketid}")
-        public Basket viewBasket(@PathVariable("basketid") String basketid) {
+    public Basket viewBasket(@PathVariable("basketid") String basketid) {
         return basketService.viewBasket(basketid);
     }
 
     @PostMapping("{basketid}/{ean}")
-        public Basket addToBasket(@PathVariable("basketid") String basketid, @PathVariable("ean") String ean) {
+    public Basket addToBasket(@PathVariable("basketid") String basketid, @PathVariable("ean") String ean) {
         return basketService.addToBasket(basketid, ean);
+    }
+
+    @GetMapping("{basketId}/score")
+    public Score getBasketScore(@PathVariable("basketId") String basketId){
+        return basketService.getBasketScore(basketId);
     }
 }
--- src/main/java/com/hackathon/howGreen/service/BasketService.java
+++ src/main/java/com/hackathon/howGreen/service/BasketService.java
@@ -2,6 +2,7 @@ package com.hackathon.howGreen.service;
 
 import com.hackathon.howGreen.domain.Basket;
 import com.hackathon.howGreen.domain.ProductInformation;
+import com.hackathon.howGreen.domain.Score;
 import com.hackathon.howGreen.repository.BasketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,9 @@ public class BasketService {
 
     @Autowired
     BasketRepository basketRepository;
+
+    @Autowired
+    ProductService productService;
 
     public Basket createBasket() {
         List<String> productList = Collections.emptyList();
@@ -31,5 +35,23 @@ public class BasketService {
         basket.add(ean);
         basketRepository.save(basket);
         return basket;
+    }
+
+    public Score getBasketScore(String basketId) {
+        Basket basket = basketRepository.findById(basketId).get();
+        return getAverageScoreOfBasket(basket);
+    }
+
+
+
+    private Score getAverageScoreOfBasket(Basket basket) {
+        int totalScore = 0;
+        for (String ean: basket.getProductList()) {
+            totalScore += productService.getProductScore(ean).getValue();
+        }
+
+        int averageScore = totalScore / basket.getProductList().size();
+
+        return new Score(averageScore);
     }
 }
